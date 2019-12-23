d9bf15e6dca20ae8e9b04114f4b310b8a30a17b5
Peter Lam <Peter.Lam@tufts.edu>
2019-12-23 17:41:04 -0500

    Added initial menu

---
+++ src/Menu.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import './Menu2.css'
+import Logo from './assets/logo.svg'
+import Github from './assets/github.jpg'
+import LinkedIn from './assets/linkedin.jpg'
+import MenuIcn from './assets/menu.svg'
+class Menu extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            page: 0, 
+            open: false, 
+            height: 0
+        }
+        // this.handleClick = this.handleClick.bind(this)
+        this.linkColor = this.linkColor.bind(this)
+    }
+
+    // handleClick() {
+    //     this.setState((prevState) => {
+    //         console.log(window.innerWidth)
+    //         if (prevState.height === 0 && window.innerWidth <= 500) {
+    //             return {open: !prevState.open, height: 85}
+    //         } else {
+    //             return {open: !prevState.open, height: 0}
+    //         }
+    //     })
+        
+    // }
+
+    // Selected page color should be dark if looked at on desktop/tablet
+    // Otherwise, the selected pages should be white
+    linkColor() {
+        if (window.innerWidth > 500) {
+            return({
+                color: "#293241"
+            })
+        } else { 
+            return({
+                color: "#FFF"
+            })
+        }
+    }
+
+    render() {
+        return(
+            <div className="Menu">
+                <div className="nav-top">
+                    <img src={Logo} alt="Personal logo" />
+                    <img className="menu-icn" src={MenuIcn} onClick={() => {this.setState({open: true})}} />
+                </div>
+                <div style={this.state.open == true ? {width: "100vw"} : {width: "0vw"}} id="myNav" className="overlay">
+                    <a href="#" className="closebtn" onClick={() => {this.setState({open: false})}}>&times;</a>
+
+                    <div class="overlay-content">
+                        <a onClick={() => this.setState({page: 0})}
+                           style={this.state.page === 0 ? this.linkColor() : {}}
+                           href="#about">Home</a>
+                        <a onClick={() => this.setState({page: 1})}
+                           style={this.state.page === 1 ? this.linkColor() : {}}
+                           href="#projects">Projects</a>
+                        <a onClick={() => this.setState({page: 2})}
+                           style={this.state.page === 2 ? this.linkColor() : {}}
+                           href="#contact">Contact</a>
+                    </div>
+                </div>
+                <div className="socials">
+                     <a href="#linkedin">
+                         <img src={LinkedIn} alt="LinkedIn Logo" />
+                     </a>
+                     <a href="#github">
+                         <img src={Github} alt="Github Logo" />
+                     </a>
+                 </div> 
+                
+            </div>
+        )
+    }
+}
+
+export default Menu 
